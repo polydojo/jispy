ba8c219322d71ebae11df0f08229b7a963ec0226
Sumukh Barve <sumukhbarve@gmail.com>
2017-09-05 00:31:00 +0530

    Minor. Added MPL2.0 notice to setupLJ.js.

--- setupLJ.js
+++ setupLJ.js
@@ -1,3 +1,18 @@
+//////////////////////////////////////////////////////////////////////////////
+//                                                                          //
+//  A library for making JavaScript environments LittleJ compatible.        //
+//  Written purely in LittleJ (i.e Jispy's JavaScript subset).              //
+//                                                                          //
+//  Copyright (c) 2017 Polydojo, Inc.                                       //
+//                                                                          //
+//                                                                          //
+//  This Source Code Form is subject to the terms of the Mozilla Public     //
+//  License, v. 2.0. If a copy of the MPL was not distributed with this     //
+//  file, You can obtain one at http://mozilla.org/MPL/2.0/.                //
+//                                                                          //
+//////////////////////////////////////////////////////////////////////////////
+
+
 (function (context) {
     'use strict';
     context.keys = Object.keys;
